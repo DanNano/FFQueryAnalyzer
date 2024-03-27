e9865d49847a456dc5ed7b21d17d133baaf43145
Daniel Laforce <93101866+DanNano@users.noreply.github.com>
2024-03-27 03:08:11 -0400

    Create Query1.js

---
+++ app/src/components/Query1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Query1() {
+    return (
+        <div>
+            <h1>Query 1</h1>
+            <p>This page will contain information about Query 1</p>
+        </div>
+    );
+}
+export default Query1;
